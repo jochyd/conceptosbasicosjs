6ff0c023a2e97692ec04759387806f59ef23877d
Jochyd <daoua.josefina@gmail.com>
2022-06-29 18:07:45 -0300

    mostramos diferentes mensajes por pantallas, consolas y alert

---
+++ js/1-variables.js
@@ -0,0 +1,24 @@
+// esto es un comentario de una unica linea
+
+/* esto es un 
+comentario de 
+multiples
+lineas
+*/
+
+// mostrar un mensaje en el documento html o pantalla
+document.write('<h1>Hola mundo, año 2022 canción</h1>');
+
+// mostrar un mensaje por consola
+console.log('alguna frase');
+
+// mostrar un mensaje por unaa ventana emergente
+// alert('esto es una ventana emergente');
+
+// mostrar por pantalla la suma de dos numeros 29 + 6 
+// cuando es numeroes mas numeros debemos poner sin comillas
+document.write('<p>Resultado: ' + (29 + 6) + '</p>');
+// Resultado: 29
+
+// Variables
+
