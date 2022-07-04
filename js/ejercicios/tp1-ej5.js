4d9c088cbaa121aa1195006dd14e0861c4974171
Jochyd <daoua.josefina@gmail.com>
2022-07-04 17:12:45 -0300

    agregue el tp1

--- js/ejercicios/tp1-ej5.js
+++ js/ejercicios/tp1-ej5.js
@@ -1,3 +1,6 @@
 let numero1 = prompt ('Ingrese un primer numero'); 
 let numero2 = prompt ('Ingrese un segundo numero');
-
+console.log(numero1);
+console.log(numero2);
+console.log(parseInt (numero1))
+document.write('El resultado es ' + (numero1) + (numero2))
