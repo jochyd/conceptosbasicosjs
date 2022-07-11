86a7c455ac4181f49021a50e62c9522709c462ec
Jochyd <daoua.josefina@gmail.com>
2022-07-11 17:21:59 -0300

    agregamos el codigo de un bucle do while

--- js/3-bucles.js
+++ js/3-bucles.js
@@ -11,13 +11,27 @@ while (condicion logica) (true o false booleana){
 //     cantidadMates = cantidadMates + 1; //cantidadmates++ todo es lo mismo que eso
 // }
 // arrancar de 0 o de 1 depende del problema que tengamos y se pone hasta <20 por lo que son 20 mates asi se cuentan
-let cantidadMates =20;
-while (cantidadMates >=1){
-    document.write('<br> Mate cebado N: ' + cantidadMates);
-    cantidadMates = cantidadMates -1; //cantidadmates-- todo es lo mismo que eso
-}
+// let cantidadMates =20;
+// while (cantidadMates >=1){
+//     document.write('<br> Mate cebado N: ' + cantidadMates);
+//     cantidadMates = cantidadMates -1; //cantidadmates-- todo es lo mismo que eso
+// }
 
 // segundo bucle do while
+//  ejemplo de bucle-while, haceer .... mientras (se cumpla la condicion logica)
 
+/*
+    do{
+        todas las lineas de codigos q quiero ejecutar varias veces
+      cambiar la condicion logica
+    }while(condicion logica)
+*/
 
+let cantidadMates = 20;
+do{
+    document.write('<br> Mate cebado N: ' + cantidadMates);
+    cantidadMates--; // cantidadMates = cantidadMates -1;
+}while( cantidadMates >=1 )
+
+// solo se cumple cuando se cumple la ccondicion logica, o solo se cumple al menos una vez
 
